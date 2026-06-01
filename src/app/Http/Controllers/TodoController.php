22cdb4dd02fb433f20fd5afc422bf3786335eabc
樋坂架蓮 <hisakakaren@toisakakarennoMacBook-Air.local>
2026-06-01 13:01:49 +0900

    TodoRequestによるバリデーションを実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 use App\Todo;
 
 class TodoController extends Controller
@@ -19,7 +19,7 @@ class TodoController extends Controller
         return view('todo.create'); 
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
     
@@ -45,16 +45,17 @@ class TodoController extends Controller
 
     public function edit($id)
     {
-        $todo = Todo::find($id);
+        $todo = $this->todo->find($id);
 
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
-        $todo = Todo::find($id);
-        $todo->fill($inputs)->save();
+        $todo = $this->todo->find($id);
+        $todo->fill($inputs);
+        $todo->save();
 
         return redirect()->route('todo.show', $todo->id);
     }
