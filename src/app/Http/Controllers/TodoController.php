25d72f1497f36d869315824ac4372dbdce3516e6
樋坂架蓮 <hisakakaren@toisakakarennoMacBook-Air.local>
2026-06-01 12:24:58 +0900

    ToDo編集内容の更新処理を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -49,5 +49,14 @@ class TodoController extends Controller
 
         return view('todo.edit', ['todo' => $todo]);
     }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        $todo = Todo::find($id);
+        $todo->fill($inputs)->save();
+
+        return redirect()->route('todo.show', $todo->id);
+    }
 }
 
